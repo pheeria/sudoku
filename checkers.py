e1ddc47642471c676c170e4b84c4efdb7a165917
pheeria <olzhas.askar@gmail.com>
2017-01-11 22:08:22 +0100

    Refactored

--- checkers.py
+++ checkers.py
@@ -1,6 +1,7 @@
 from cells import *
 from board import print_board
 
+
 def linear_check(line, possibilities):
     for num in line:
         if num != 0 and num in possibilities:
@@ -8,8 +9,8 @@ def linear_check(line, possibilities):
 
 
 def quadrant_check(board, i, j, possibilities):
-    x = choose(i)
-    y = choose(j)
+    x = find_quadrant(i)
+    y = find_quadrant(j)
     for row in range(x, x + 3):
         for col in range(y, y + 3):
             cell = board[row][col]
@@ -17,7 +18,7 @@ def quadrant_check(board, i, j, possibilities):
                 possibilities.remove(cell)
 
 
-def choose(x):
+def find_quadrant(x):
     if x > 5:
         return 6
     elif x > 2:
